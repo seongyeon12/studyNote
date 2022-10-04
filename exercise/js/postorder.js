d3962b39c1c31e69427ee4e1e08b916e2e615676
seongyeon12 <k.seongyeon12@gmail.com>
2022-10-04 21:03:38 +0900

    2022/10/04

--- exercise/js/postorder.js
+++ exercise/js/postorder.js
@@ -8,12 +8,42 @@ function BinaryTree() {
   this.root = null;
 } 
 
-BinaryTree.prototype.add = function(node) {
-  
+BinaryTree.prototype._insertNode = function(node, value) {
+  if (node === null) {
+    node = new Node(value);
+  } else if (value < node.value) {
+    node.left = this._insertNode(node.left, value);
+  } else if (value > node.value) {
+    node.right = this._insertNode(node.right, value);
+  }
+
+  return node;
+}
+
+BinaryTree.prototype._insert = function (value) {
+  this.root = this._insertNode(this.root, value)
+}
+
+BinaryTree.prototype.postOrderTraverse = function (node, array) {
+  if (node === null) {
+    return;
+  }
+
+  this.postOrderTraverse(node.left, array)
+  this.postOrderTraverse(node.right, array)
+  array.push(node.value)
 }
 
 function answer(str) {
   let result = [];
+
+  let tree = new BinaryTree();
+  for (let i = 0; i < str.length; i++) {
+    tree._insert(str[i])
+  }
+
+  tree.postOrderTraverse(tree.root, result);
+
   return result;
 }
 
