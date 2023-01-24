e6dc8b18cd01733454b67669b33397791e5dfc55
seongyeon12 <k.seongyeon12@gmail.com>
2023-01-25 04:30:51 +0900

    no message

--- materialization/nomadCoder/movie/src/routes/Detail.js
+++ materialization/nomadCoder/movie/src/routes/Detail.js
@@ -3,6 +3,17 @@ import {useParams} from 'react-router-dom'
 
 function Detail() {
   const {id} = useParams()
+  const alist = []
+
+  useEffect(() => {
+		const fetchData = async() => {
+          const res = await fetch(`http://52.194.161.226:8080/`);
+          const result = res.json();
+          return result;
+        }	
+        
+        fetchData().then(res => alist(res));
+    });
 
   const getMovie = async () => {
     const json = await(
@@ -10,7 +21,8 @@ function Detail() {
     ).json()
     console.log(json)
   }
-  
+
+    console.log(alist)
   useEffect(() => {
     getMovie()
   }, [])
